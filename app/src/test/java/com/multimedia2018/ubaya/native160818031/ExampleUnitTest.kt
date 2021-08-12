91d2cbda12b9ce0b450da8704886af79c8b34799
GeraldiWinson <s16081801@student.ubaya.ac.id>
2021-08-12 13:57:05 +0700

    Ver 0.1 First time repo activation and usage Initialize commit

---
+++ app/src/test/java/com/multimedia2018/ubaya/native160818031/ExampleUnitTest.kt
@@ -0,0 +1,17 @@
+package com.multimedia2018.ubaya.native160818031
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
